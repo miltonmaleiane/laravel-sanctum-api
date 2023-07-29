e36abe0d9c9dd9c416c210a0339d1fe18b85fc85
miltonmaleiane <miltonmaleiane1@gmail.com>
2023-07-29 09:44:44 -0700

    Product Update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -42,7 +42,9 @@ class ProductController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+       $product = Product::find($id);
+       $product->update($request->all());
+       return $product; 
     }
 
     /**
