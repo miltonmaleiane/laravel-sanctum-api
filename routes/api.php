083592d9f6b9cd1d7b563634d5d5030b3b100779
miltonmaleiane <miltonmaleiane1@gmail.com>
2023-08-01 09:55:02 -0700

    Protecting routes

--- routes/api.php
+++ routes/api.php
@@ -16,11 +16,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+//rotas publicas
 Route::resource('products',ProductController::class);
-Route::get('/products/search/{name}',[ProductController::class, 'search']);
+
 //Route::get('/products',[ProductController::class, 'index']);
 //Route::get('/products/$id',[ProductController::class, 'show']);
 //Route::post ('/products', [ProductController::class, 'store']); 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+// rotas protegidas
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/products/search/{name}',[ProductController::class, 'search']);
 });
