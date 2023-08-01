7d0a7f3ffdffde72bbcdb51e2e0489a52dd2a7d0
miltonmaleiane <miltonmaleiane1@gmail.com>
2023-08-01 10:20:04 -0700

    User fields

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,8 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\User;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash; 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request){
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields ['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+        
+    }
 }
